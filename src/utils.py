3975cfd270cd41cc7e3d2b7c495953dc569d851a
zhaosiheng <34878280+zhaosiheng@users.noreply.github.com>
2021-06-02 15:51:13 +0800

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -6,11 +6,11 @@ import torch.nn.functional as F
 
 class NodeDistance:
 
-    def __init__(self, adj, nclass=4):
+    def __init__(self, n_node, edges, nclass=4):
         """
         :param graph: Networkx Graph.
         """
-        self.adj = adj
+        
         self.graph = nx.from_scipy_sparse_matrix(adj)
         self.nclass = nclass
 
@@ -27,38 +27,14 @@ class NodeDistance:
         self.distance = distance
         return torch.LongTensor(distance) - 1
 
-class Base:
-
-    def __init__(self, adj, features, device):
-        self.adj = adj
-        self.features = features.to(device)
-        self.device = device
-        self.cached_adj_norm = None
-
-    def get_adj_norm(self):
-        if self.cached_adj_norm is None:
-            adj_norm = preprocess_adj(self.adj, self.device)
-            self.cached_adj_norm= adj_norm
-        return self.cached_adj_norm
-
-    def make_loss(self, embeddings):
-        return 0
-
-    def transform_data(self):
-        return self.get_adj_norm(), self.features
      
 class PairwiseDistance(Base):
 
-    def __init__(self, adj, features, nhid, device, idx_train, regression=False):
+    def __init__(self, adj, nhid, device, regression=False):
         self.adj = adj
-        self.features = features.to(device)
-        self.nfeat = features.shape[1]
+
         self.cached_adj_norm = None
         self.device = device
-
-        self.labeled = idx_train.cpu().numpy()
-        self.all = np.arange(adj.shape[0])
-        self.unlabeled = np.array([n for n in self.all if n not in idx_train])
 
         self.regression = regression
         self.nclass = 4
@@ -69,8 +45,6 @@ class PairwiseDistance(Base):
 
         self.pseudo_labels = None
 
-    def transform_data(self):
-        return self.get_adj_norm(), self.features
 
     def make_loss(self, embeddings):
         if self.regression:
@@ -107,22 +81,5 @@ class PairwiseDistance(Base):
         node_pairs = np.array(node_pairs).reshape(-1, 2).transpose()
         return node_pairs[0], node_pairs[1]
 
-    def _sample(self, labels, ratio=0.1, k=400):
-        # first sample k nodes
-        candidates = self.all
-        # perm = np.random.choice(candidates, int(ratio*len(candidates)), replace=False)
-        perm = np.random.choice(candidates, 300, replace=False)
 
-        node_pairs = []
-        # then sample k other nodes to make sure class balance
-        for i in range(1, labels.max()+1):
-            tmp = np.array(np.where(labels==i)).transpose()
-            tmp_0 = tmp[:, 0]
-            targets = np.where(tmp_0.reshape(tmp_0.size, 1) == perm)[0]
-            # targets = np.array([True if x in perm else False for x in tmp[:, 0]])
-            # indices = np.random.choice(np.arange(len(tmp))[targets], k, replace=False)
-            indices = np.random.choice(targets, k, replace=False)
-            node_pairs.append(tmp[indices])
-        node_pairs = np.array(node_pairs).reshape(-1, 2).transpose()
-        return node_pairs[0], node_pairs[1]
 
