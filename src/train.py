1493835347578a567af06043438e5a7d44113374
zhaosiheng <34878280+zhaosiheng@users.noreply.github.com>
2021-06-02 21:40:50 +0800

    Update train.py

--- src/train.py
+++ src/train.py
@@ -30,7 +30,7 @@ def forward(ssl, para, model, loader, device, writer, epoch, top_k=20, optimizer
         if para.task_node:
             loss += model.loss_nodes(h, edges, device)
         if para.ssl_task:
-            loss += ssl.make_loss(h)
+            loss += ssl.make_loss(h, edges)
         if train_flag:
             loss.backward()
             optimizer.step()
