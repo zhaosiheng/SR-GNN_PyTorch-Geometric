fbf5929defdddbe60586b67d1b3a22aa79a3b85d
zhaosiheng <34878280+zhaosiheng@users.noreply.github.com>
2021-06-03 21:00:44 +0800

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -80,7 +80,7 @@ class PairwiseDistance():
         for i in range(1, labels.max()+1):
             tmp = np.array(np.where(labels==i)).transpose()
        #     indices = np.random.choice(np.arange(len(tmp)), k, replace=False)
-            indices = np.random.choice(np.arange(len(tmp)), len(tmp)/2, replace=False)
+            indices = np.random.choice(np.arange(len(tmp)), int(len(tmp)/2), replace=False)
             node_pairs.append(tmp[indices])
         node_pairs = np.array(node_pairs).reshape(-1, 2).transpose()
         return node_pairs[0], node_pairs[1]
